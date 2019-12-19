7b5c3674ddf5618c84a0e836adbd82de450ac8d9
sttng <46765778+sttng@users.noreply.github.com>
2019-12-19 10:00:11 +0800

    Update sd_prep.cmd

--- sd_prep.cmd
+++ sd_prep.cmd
@@ -2,36 +2,51 @@
 mkdir SD_prep
 cd SD_prep
 
-REM Getting Hekate latest version
-for /f "tokens=2" %%i in ('curl -s https://api.github.com/repos/CTCaer/hekate/releases/latest ^| findstr browser_download_url ^| findstr hekate_ctc') do set RESULT=%%i
-FOR /F "delims=" %%I IN (%RESULT%) DO SET URL_hekate_latest=%%I
-for %%F in ("%URL_hekate_latest%") DO SET hekate_latest=%%~nxF
-echo Getting Hekate latest version: %hekate_latest%
-curl -L -O -# %URL_hekate_latest%
+REM Getting Atmosphere latest version
+SET my_url=https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases/latest
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr browser_download_url ^| findstr zip') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET URL_latest=%%I
+for %%F in ("%URL_latest%") DO SET file_latest=%%~nxF
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr tag_name') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET tag_name=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr name ^| findstr zip') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET name=%%I
+echo Getting %name% version: %tag_name%
+curl -L -O -# %URL_latest%
 echo.
+tar -xf %file_latest%
+
+REM Getting Hekate latest version
+SET my_url=https://api.github.com/repos/CTCaer/hekate/releases/latest
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr browser_download_url ^| findstr hekate_ctc') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET URL_latest=%%I
+for %%F in ("%URL_latest%") DO SET file_latest=%%~nxF
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr tag_name') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET tag_name=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr name ^| findstr hekate_ctc') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET name=%%I
+echo Getting %name% version: %tag_name%
+curl -L -O -# %URL_latest%
+echo.
+tar -xf %file_latest%
 
 REM Getting Hekate additions: hekate_ipl.ini and bootlogos.zip
 echo Getting Hekate additions: hekate_ipl.ini and bootlogos.zip
 curl -L -O -# https://nh-server.github.io/switch-guide/files/emu/hekate_ipl.ini
 curl -L -O -# https://nh-server.github.io/switch-guide/files/bootlogos.zip
 echo.
-
-REM Getting Atmosphere latest version
-for /f "tokens=2" %%i in ('curl -s https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases/latest ^| findstr browser_download_url ^| findstr atmosphere') do set RESULT=%%i
-FOR /F "delims=" %%I IN (%RESULT%) DO SET URL_atmosphere_latest=%%I
-for %%F in ("%URL_atmosphere_latest%") DO SET atmosphere_latest=%%~nxF
-echo Getting Atmosphere latest version: %atmosphere_latest%
-curl -L -O -# %URL_atmosphere_latest%
-echo.
-
-REM Unpacking Atmosphere, Hekate, Hekate Logos
-tar -xf %atmosphere_latest%
-tar -xf %hekate_latest%
 tar -xf bootlogos.zip
 
 REM Getting Lockpick latest version
-echo Getting Lockpick latest version: Lockpick_RCM.bin
-curl -L -O -# https://github.com/shchmue/Lockpick_RCM/releases/latest/download/Lockpick_RCM.bin
+SET my_url=https://api.github.com/repos/shchmue/Lockpick_RCM/releases/latest
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr browser_download_url') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET URL_latest=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr tag_name') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET tag_name=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr name') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET name=%%I
+echo Getting %name% version: %tag_name%
+curl -L -O -# %URL_latest%
 echo.
 
 move hekate_ipl.ini bootloader >NUL
@@ -40,16 +55,46 @@ move atmosphere\reboot_payload.bin atmosphere\#reboot_payload.bin >NUL
 copy hekate_ctcaer_*.bin bootloader\payloads >NUL
 move hekate_ctcaer_*.bin atmosphere\reboot_payload.bin >NUL
 
-REM Getting additonal tools: ChoiDujourNXv102, Incognito_RCM (latest), nxdumptool (latest)
-echo Getting additonal tools: ChoiDujourNXv102, Incognito_RCM (latest), nxdumptool (latest)
+REM Getting ChoiDujourNXv102
+echo Getting ChoiDujourNXv102
 curl -L -O -# https://files.sshnuke.net/ChoiDujourNXv102.zip
-curl -L -O -# https://github.com/jimzrt/Incognito_RCM/releases/latest/download/Incognito_RCM.bin
-curl -L -O -# https://github.com/DarkMatterCore/nxdumptool/releases/latest/download/nxdumptool.nro
+echo.
+
+REM Getting Incognito_RCM latest version
+SET my_url=https://api.github.com/repos/jimzrt/Incognito_RCM/releases/latest
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr browser_download_url') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET URL_latest=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr tag_name') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET tag_name=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr name') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET name=%%I
+echo Getting %name% version: %tag_name%
+curl -L -O -# %URL_latest%
+echo.
+
+REM Getting nxdumptool latest version
+SET my_url=https://api.github.com/repos/DarkMatterCore/nxdumptool/releases/latest
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr browser_download_url') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET URL_latest=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr tag_name') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET tag_name=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr name ^| findstr nro') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET name=%%I
+echo Getting %name% version: %tag_name%
+curl -L -O -# %URL_latest%
 echo.
 
 REM Getting EdiZon latest version
-echo Getting EdiZon latest version: SD.zip
-curl -L -O -# https://github.com/WerWolv/EdiZon/releases/latest/download/SD.zip
+SET my_url=https://api.github.com/repos/WerWolv/EdiZon/releases/latest
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr browser_download_url') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET URL_latest=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr tag_name') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET tag_name=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr name ^| findstr zip') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET name=%%I
+echo Getting %name% version: %tag_name%
+curl -L -O -# %URL_latest%
+echo.
 tar -xf SD.zip
 
 move Incognito_RCM.bin bootloader\payloads >NUL
@@ -59,28 +104,68 @@ move *.txt switch >NUL
 move nxdumptool.nro switch >NUL
 
 REM Getting FTPD latest version
-echo Getting FTPD latest version: FTPD.nro
+SET my_url=https://api.github.com/repos/mtheall/ftpd/releases/latest
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr browser_download_url ^| findstr nro') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET URL_latest=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr tag_name') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET tag_name=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr name ^| findstr nro') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET name=%%I
+echo Getting %name% version: %tag_name%
 curl -L -O -# https://github.com/mtheall/ftpd/releases/latest/download/ftpd.nro
 move ftpd.nro switch >NUL
+echo.
 
 REM Getting NXThemeInstaller latest version
-echo Getting NXThemeInstaller latest version: NXThemesInstaller.nro
-curl -L -O -# https://github.com/exelix11/SwitchThemeInjector/releases/latest/download/NXThemesInstaller.nro
+SET my_url=https://api.github.com/repos/exelix11/SwitchThemeInjector/releases/latest
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr browser_download_url ^| findstr nro') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET URL_latest=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr tag_name') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET tag_name=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr name ^| findstr nro') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET name=%%I
+echo Getting %name% version: %tag_name%
+curl -L -O -# %URL_latest%
+echo.
 move NXThemesInstaller.nro switch >NUL
 
 REM Getting NX-Shell latest version
-echo Getting NX-Shell latest version: NX-Shell.nro
-curl -L -O -# https://github.com/joel16/NX-Shell/releases/latest/download/NX-Shell.nro
+SET my_url=https://api.github.com/repos/joel16/NX-Shell/releases/latest
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr browser_download_url') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET URL_latest=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr tag_name') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET tag_name=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr name') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET name=%%I
+echo Getting %name% version: %tag_name%
+curl -L -O -# %URL_latest%
+echo.
 move NX-Shell.nro switch >NUL
 
 REM Getting atmosphere-updater latest version
-echo Getting atmosphere-updater latest version: atmosphere-updater.nro
-curl -L -O -# https://github.com/ITotalJustice/atmosphere-updater/releases/latest/download/atmosphere-updater.nro
+SET my_url=https://api.github.com/repos/ITotalJustice/atmosphere-updater/releases/latest
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr browser_download_url') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET URL_latest=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr tag_name') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET tag_name=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr name') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET name=%%I
+echo Getting %name% version: %tag_name%
+curl -L -O -# %URL_latest%
+echo.
 move atmosphere-updater.nro switch >NUL
 
 REM Getting hbappstore latest version
-echo Getting hbappstore latest version: appstore.nro
-curl -L -O -# https://github.com/vgmoose/hb-appstore/releases/latest/download/appstore.nro
+SET my_url=https://api.github.com/repos/vgmoose/hb-appstore/releases/latest
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr browser_download_url ^| findstr nro') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET URL_latest=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr tag_name') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET tag_name=%%I
+for /f "tokens=2" %%i in ('curl -s %my_url% ^| findstr name ^| findstr nro') do set RESULT=%%i
+for /f "delims=" %%I IN (%RESULT%) DO SET name=%%I
+echo Getting %name% version: %tag_name%
+curl -L -O -# %URL_latest%
+echo.
 cd switch
 mkdir appstore
 cd ..
@@ -102,7 +187,7 @@ cd emummc
 (echo [emummc]
 echo enabled = 1 
 echo sector = 0x2
-echo nintendo_path = Emutendo) > filename.txt
+echo nintendo_path = Emutendo) > emummc.ini
 cd..
 
 del *.zip
